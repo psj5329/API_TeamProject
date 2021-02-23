3da22b66b2126401c257d0ba1b43bbb242d230fd
rltk <79186373+rltk@users.noreply.github.com>
2021-02-23 12:50:27 +0900

    [LHJ] player 2.23

--- Momodora/Momodora/Player.cpp
+++ Momodora/Momodora/Player.cpp
@@ -11,6 +11,9 @@ void Player::Init()
 
 	IMAGEMANAGER->LoadFromFile(L"Run", Resources(L"Player/run"), 490, 96, 10, 2, true);
 	mRunImage = IMAGEMANAGER->FindImage(L"Run");
+	
+	IMAGEMANAGER->LoadFromFile(L"Turn", Resources(L"/Player/turn"), 147, 96, 3, 2, true);
+	mTurnImage = IMAGEMANAGER->FindImage(L"Turn");
 
 	IMAGEMANAGER->LoadFromFile(L"Jump", Resources(L"/Player/jump"), 147, 96, 3, 2, true);
 	mJumpImage = IMAGEMANAGER->FindImage(L"Jump");
@@ -24,6 +27,16 @@ void Player::Init()
 	IMAGEMANAGER->LoadFromFile(L"Rise", Resources(L"/Player/rise"), 98, 96, 2, 2, true);
 	mRiseImage = IMAGEMANAGER->FindImage(L"Rise");
 
+	IMAGEMANAGER->LoadFromFile(L"Roll", Resources(L"/Player/roll"), 392, 96, 8, 2, true);
+	mRollImage = IMAGEMANAGER->FindImage(L"Roll");
+
+	IMAGEMANAGER->LoadFromFile(L"LadderUp", Resources(L"/Player/ladderup"), 204, 48, 6, 1, true);
+	mLadderUpImage = IMAGEMANAGER->FindImage(L"LadderUp");
+
+	IMAGEMANAGER->LoadFromFile(L"LadderDown", Resources(L"/Player/ladderdown"), 105, 43, 3, 1, true);
+	mLadderDownImage = IMAGEMANAGER->FindImage(L"LadderDown");
+
+
 	//스탠드 이미지
 	mLeftIdleAnimation = new Animation();
 	mLeftIdleAnimation->InitFrameByStartEnd(0, 1, 5, 1, true);
@@ -36,26 +49,44 @@ void Player::Init()
 	mRightIdleAnimation->SetIsLoop(true);
 	mRightIdleAnimation->SetFrameUpdateTime(0.2f);
 	mRightIdleAnimation->Play();
-
 	//이동 이미지
 	mLeftRunStartAnimation = new Animation();
-	mLeftRunStartAnimation->InitFrameByStartEnd(0, 1, 1, 1, true); //유찬
+	mLeftRunStartAnimation->InitFrameByStartEnd(8, 1, 9, 1, true);
 	mLeftRunStartAnimation->SetIsLoop(false);
 	mLeftRunStartAnimation->SetFrameUpdateTime(0.1f);
 	mLeftRunStartAnimation->Play();
+	mLeftRunStartAnimation->SetCallbackFunc(bind(&Player::SetStateRun, this));
 
 	mLeftRunAnimation = new Animation();
-	mLeftRunAnimation->InitFrameByStartEnd(0, 1, 9, 1, true);
+	mLeftRunAnimation->InitFrameByStartEnd(0, 1, 7, 1, true);
 	mLeftRunAnimation->SetIsLoop(true);
 	mLeftRunAnimation->SetFrameUpdateTime(0.1f);
 	mLeftRunAnimation->Play();
 
+	mRightRunStartAnimation = new Animation();
+	mRightRunStartAnimation->InitFrameByStartEnd(0, 0, 1, 0, true);
+	mRightRunStartAnimation->SetIsLoop(false);
+	mRightRunStartAnimation->SetFrameUpdateTime(0.1f);
+	mRightRunStartAnimation->Play();
+	mRightRunStartAnimation->SetCallbackFunc(bind(&Player::SetStateRun, this));
+
 	mRightRunAnimation = new Animation();
-	mRightRunAnimation->InitFrameByStartEnd(0, 0, 9, 0, false);
+	mRightRunAnimation->InitFrameByStartEnd(2, 0, 9, 0, false);
 	mRightRunAnimation->SetIsLoop(true);
 	mRightRunAnimation->SetFrameUpdateTime(0.1f);
 	mRightRunAnimation->Play();
+	//방향전환 이미지
+	mLeftTurnAnimation = new Animation();
+	mLeftTurnAnimation->InitFrameByStartEnd(0, 1, 2, 1, true);
+	mLeftTurnAnimation->SetIsLoop(false);
+	mLeftTurnAnimation->SetFrameUpdateTime(0.3f);
+	mLeftTurnAnimation->Play();
 
+	mRightTurnAnimation = new Animation();
+	mRightTurnAnimation->InitFrameByStartEnd(0, 0, 2, 0, false);
+	mRightTurnAnimation->SetIsLoop(false);
+	mRightTurnAnimation->SetFrameUpdateTime(0.3f);
+	mRightTurnAnimation->Play();
 	//점프 이미지
 	mLeftJumpAnimation = new Animation();
 	mLeftJumpAnimation->InitFrameByStartEnd(0, 1, 2, 1, true);
@@ -68,7 +99,6 @@ void Player::Init()
 	mRightJumpAnimation->SetIsLoop(true);
 	mRightJumpAnimation->SetFrameUpdateTime(0.3f);
 	mRightJumpAnimation->Play();
-
 	//점프 하강 이미지
 	mLeftFallAnimation = new Animation();
 	mLeftFallAnimation->InitFrameByStartEnd(0, 1, 4, 1, true);
@@ -81,49 +111,56 @@ void Player::Init()
 	mRightFallAnimation->SetIsLoop(true);
 	mRightFallAnimation->SetFrameUpdateTime(0.3f);
 	mRightFallAnimation->Play();
-
 	//앉기 이미지
 	mLeftCrouchAnimation = new Animation();
 	mLeftCrouchAnimation->InitFrameByStartEnd(0, 1, 3, 1, true);
 	mLeftCrouchAnimation->SetIsLoop(false);
-	mLeftCrouchAnimation->SetFrameUpdateTime(0.3f);
+	mLeftCrouchAnimation->SetFrameUpdateTime(0.1f);
 	mLeftCrouchAnimation->Play();
 
 	mRightCrouchAnimation = new Animation();
 	mRightCrouchAnimation->InitFrameByStartEnd(0, 0, 3, 0, false);
 	mRightCrouchAnimation->SetIsLoop(false);
-	mRightCrouchAnimation->SetFrameUpdateTime(0.3f);
+	mRightCrouchAnimation->SetFrameUpdateTime(0.1f);
 	mRightCrouchAnimation->Play();
-
 	//일어나기 이미지
 	mLeftRiseAnimation = new Animation();
 	mLeftRiseAnimation->InitFrameByStartEnd(0, 1, 1, 1, true);
 	mLeftRiseAnimation->SetIsLoop(false);
-	mLeftRiseAnimation->SetFrameUpdateTime(0.3f);
+	mLeftRiseAnimation->SetFrameUpdateTime(0.1f);
 	mLeftRiseAnimation->Play();
 
 	mRightRiseAnimation = new Animation();
 	mRightRiseAnimation->InitFrameByStartEnd(0, 0, 1, 0, false);
 	mRightRiseAnimation->SetIsLoop(false);
-	mRightRiseAnimation->SetFrameUpdateTime(0.3f);
+	mRightRiseAnimation->SetFrameUpdateTime(0.1f);
 	mRightRiseAnimation->Play();
+	//구르기 이미지
+	mLeftRollAnimation = new Animation();
+	mLeftRollAnimation->InitFrameByStartEnd(0, 1, 7, 1, true);
+	mLeftRollAnimation->SetIsLoop(false);
+	mLeftRollAnimation->SetFrameUpdateTime(0.1f);
+	mLeftRollAnimation->Play();
+
+	mRightRollAnimation = new Animation();
+	mRightRollAnimation->InitFrameByStartEnd(0, 0, 7, 0, false);
+	mRightRollAnimation->SetIsLoop(false);
+	mRightRollAnimation->SetFrameUpdateTime(0.1f);
+	mRightRollAnimation->Play();
 
 	//값 설정
 	mX = WINSIZEX / 2;
 	mY = WINSIZEY / 2;
-	mSpeed = 200.f;
-	mGravity = 0.2f;
-	mState = State::Right;
 	mCurrentAnimation = mRightIdleAnimation;
 	mCurrentImage = mIdleImage;
-	mSizeX = (float)(mIdleImage->GetFrameWidth());
-	mSizeY = (float)(mIdleImage->GetFrameHeight());
+	mSizeX = (float)(mIdleImage->GetFrameWidth()) *2;
+	mSizeY = (float)(mIdleImage->GetFrameHeight()) *2;
 	mRect = RectMakeCenter((int)mX, (int)mY, (int)mSizeX, (int)mSizeY);
 }
 
 void Player::Release()
 {
-	
+
 }
 
 void Player::Update()
@@ -131,14 +168,15 @@ void Player::Update()
 	//이동 프레임
 	if (Input::GetInstance()->GetKeyDown(VK_LEFT))
 	{
-		mState = State::Left;
+		mState = State::LeftRun;
 		mCurrentAnimation->Stop();
-		mCurrentAnimation = mLeftRunAnimation;
+		mCurrentAnimation = mLeftRunStartAnimation;
 		mCurrentAnimation->Play();
 		mCurrentImage = mRunImage;
 	}
 	if (Input::GetInstance()->GetKeyUp(VK_LEFT))
 	{
+		mState = State::LeftIdle;
 		mCurrentAnimation->Stop();
 		mCurrentAnimation = mLeftIdleAnimation;
 		mCurrentAnimation->Play();
@@ -146,135 +184,201 @@ void Player::Update()
 	}
 	if (Input::GetInstance()->GetKeyDown(VK_RIGHT))
 	{
-		mState = State::Right;
+		mState = State::RightRun;
 		mCurrentAnimation->Stop();
-		mCurrentAnimation = mRightRunAnimation;
+		mCurrentAnimation = mRightRunStartAnimation;
 		mCurrentAnimation->Play();
 		mCurrentImage = mRunImage;
 	}
 	if (Input::GetInstance()->GetKeyUp(VK_RIGHT))
 	{
+		mState = State::RightIdle;
 		mCurrentAnimation->Stop();
 		mCurrentAnimation = mRightIdleAnimation;
 		mCurrentAnimation->Play();
 		mCurrentImage = mIdleImage;
 	}
-	//점프 프레임
+	//이동 구현 및 방향전환
+	if (Input::GetInstance()->GetKey(VK_LEFT))
+	{
+		if (stopmove == 0)
+		{
+			mX -= mSpeed * Time::GetInstance()->DeltaTime();
+
+			if (Input::GetInstance()->GetKeyDown(VK_RIGHT))
+			{
+				mState = State::RightRun;
+				mCurrentAnimation->Stop();
+				mCurrentAnimation = mRightTurnAnimation;
+				mCurrentAnimation->Play();
+				mCurrentImage = mTurnImage;
+			}
+		}
+	}
+	if (Input::GetInstance()->GetKey(VK_RIGHT))
+	{
+		if (stopmove == 0)
+		{
+			mX += mSpeed * Time::GetInstance()->DeltaTime();
+
+			if (Input::GetInstance()->GetKeyDown(VK_LEFT))
+			{
+				mState = State::LeftRun;
+				mCurrentAnimation->Stop();
+				mCurrentAnimation = mLeftTurnAnimation;
+				mCurrentAnimation->Play();
+				mCurrentImage = mTurnImage;
+			}
+		}
+	}
+
+	//점프
 	if (Input::GetInstance()->GetKeyDown(VK_SPACE))
 	{
 		mJumpPower = 8.f;
 
 		if (mJumpPower > 0)
 		{
-			if (mState == State::Right)
+			if (mState == State::LeftIdle || mState == State::LeftRun)
 			{
+				mState = State::LeftJump;
 				mCurrentAnimation->Stop();
-				mCurrentAnimation = mRightJumpAnimation;
+				mCurrentAnimation = mLeftJumpAnimation;
 				mCurrentAnimation->Play();
 				mCurrentImage = mJumpImage;
 			}
-			if (mState == State::Left)
+			if (mState == State::RightIdle || mState == State::RightRun)
 			{
+				mState = State::RightJump;
 				mCurrentAnimation->Stop();
-				mCurrentAnimation = mLeftJumpAnimation;
+				mCurrentAnimation = mRightJumpAnimation;
 				mCurrentAnimation->Play();
 				mCurrentImage = mJumpImage;
 			}
 		}
 		if (mJumpPower < 0)
 		{
-			if (mState == State::Right)
-			{
-				mCurrentAnimation->Stop();
-				mCurrentAnimation = mRightFallAnimation;
-				mCurrentAnimation->Play();
-				mCurrentImage = mFallImage;
-			}
-			if (mState == State::Left)
+			if (mState == State::LeftJump)
 			{
 				mCurrentAnimation->Stop();
 				mCurrentAnimation = mLeftFallAnimation;
 				mCurrentAnimation->Play();
 				mCurrentImage = mFallImage;
 			}
+			if (mState == State::RightJump)
+			{
+				mCurrentAnimation->Stop();
+				mCurrentAnimation = mRightFallAnimation;
+				mCurrentAnimation->Play();
+				mCurrentImage = mFallImage;
+			}
 		}
 	}
-	//점프
 	mY -= mJumpPower;
 	mJumpPower -= mGravity;
 	if (mY > WINSIZEY / 2)
 	{
 		mJumpPower = 0;
-		//mCurrentImage = mIdleImage;
 	}
 
-	//앉기 프레임
+	//앉기
 	if (Input::GetInstance()->GetKeyDown('C'))
 	{
-		mRect.top += 24;
+		stopmove = 1;
 
-		if (mState == State::Right)
+		if (mState == State::LeftIdle || mState == State::LeftRun)
 		{
-			mCurrentAnimation->Stop();
-			mCurrentAnimation = mRightCrouchAnimation;
-			mCurrentAnimation->Play();
-			mCurrentImage = mCrouchImage;
-		}
-		if (mState == State::Left)
-		{
+			mState = State::LeftCrouch;
 			mCurrentAnimation->Stop();
 			mCurrentAnimation = mLeftCrouchAnimation;
 			mCurrentAnimation->Play();
 			mCurrentImage = mCrouchImage;
 		}
+		if (mState == State::RightIdle || mState == State::RightRun)
+		{
+			mState = State::RightCrouch;
+			mCurrentAnimation->Stop();
+			mCurrentAnimation = mRightCrouchAnimation;
+			mCurrentAnimation->Play();
+			mCurrentImage = mCrouchImage;
+		}
 	}
-	//일어서기 프레임
+	//일어서기
 	if (Input::GetInstance()->GetKeyUp('C'))
 	{
-		mRect.top -= 24;
+		stopmove = 0;
 
-		if (mState == State::Right)
-		{
-			mCurrentAnimation->Stop();
-			mCurrentAnimation = mRightRiseAnimation;
-			mCurrentAnimation->Play();
-			mCurrentImage = mRiseImage;
-		}
-		if (mState == State::Left)
+		if (mState == State::LeftCrouch)
 		{
 			mCurrentAnimation->Stop();
 			mCurrentAnimation = mLeftRiseAnimation;
 			mCurrentAnimation->Play();
 			mCurrentImage = mRiseImage;
 		}
+		if (mState == State::RightCrouch)
+		{
+			mCurrentAnimation->Stop();
+			mCurrentAnimation = mRightRiseAnimation;
+			mCurrentAnimation->Play();
+			mCurrentImage = mRiseImage;
+		}
 	}
 
-	//이동
-	if (Input::GetInstance()->GetKey(VK_LEFT))
+	//구르기
+	if (Input::GetInstance()->GetKeyDown(VK_LSHIFT))
 	{
-		mX -= mSpeed * Time::GetInstance()->DeltaTime();
+		if (mState == State::LeftIdle || mState == State::LeftRun)
+		{
+			mX -= mSpeed * Time::GetInstance()->DeltaTime() * 2;
+			mRect = RectMakeCenter(mX, mY, mSizeX, mSizeY);
+
+			mState = State::Roll;
+			mCurrentAnimation->Stop();
+			mCurrentAnimation = mLeftRollAnimation;
+			mCurrentAnimation->Play();
+			mCurrentImage = mRollImage;
+		}
+		if (mState == State::RightIdle || mState == State::RightRun)
+		{
+			mX += mSpeed * Time::GetInstance()->DeltaTime() * 2;
+			mRect = RectMakeCenter(mX, mY, mSizeX, mSizeY);
+
+			mState = State::Roll;
+			mCurrentAnimation->Stop();
+			mCurrentAnimation = mRightRollAnimation;
+			mCurrentAnimation->Play();
+			mCurrentImage = mRollImage;
+		}
 	}
-	if (Input::GetInstance()->GetKey(VK_RIGHT))
+
+	if (stopmove == 0)
 	{
-		mX += mSpeed * Time::GetInstance()->DeltaTime();
+		mRect = RectMakeCenter(mX, mY, mSizeX, mSizeY);
 	}
-	//앉기
-	//if (Input::GetInstance()->GetKeyDown('C'))
-	//{
-	//	mRect.top += 24;
-	//}
-	//if (Input::GetInstance()->GetKeyUp('C'))
-	//{
-	//	mRect.top -= 24;
-	//	mCurrentImage = mIdleImage;
-	//}
-
-	mRect = RectMakeCenter(mX, mY, mSizeX, mSizeY);
-
 	mCurrentAnimation->Update();
 }
 
 void Player::Render(HDC hdc)
 {
-	CameraManager::GetInstance()->GetMainCamera()->AlphaScaleFrameRender(hdc, mCurrentImage, (int)mRect.left, (int)mRect.top, mCurrentAnimation->GetNowFrameX(), mCurrentAnimation->GetNowFrameY(), (int)mSizeX * 2, (int)mSizeY * 2, 1.f);
+	CAMERAMANAGER->GetMainCamera()->RenderRectInCamera(hdc, mRect);
+	CameraManager::GetInstance()->GetMainCamera()->AlphaScaleFrameRender(hdc, mCurrentImage, (int)mRect.left, (int)mRect.top, mCurrentAnimation->GetNowFrameX(), mCurrentAnimation->GetNowFrameY(), (int)mSizeX, (int)mSizeY , 1.f);
+	//RenderRect(hdc, mRect);
+}
+
+void Player::SetStateRun()
+{
+	if (mState == State::LeftRun)
+	{
+		mCurrentAnimation->Stop();
+		mCurrentAnimation = mLeftRunAnimation;
+		mCurrentAnimation->Play();
+		mCurrentImage = mRunImage;
+	}
+	else if (mState == State::RightRun)
+	{
+		mCurrentAnimation->Stop();
+		mCurrentAnimation = mRightRunAnimation;
+		mCurrentAnimation->Play();
+		mCurrentImage = mRunImage;
+	}
 }
--- Momodora/Momodora/Player.h
+++ Momodora/Momodora/Player.h
@@ -2,10 +2,20 @@
 
 #include "GameObject.h"
 
-enum class State : int	//추가
+enum class State : int
 {
-	Left,
-	Right
+	LeftIdle,
+	LeftRun,
+	LeftJump,
+	LeftCrouch,
+
+	RightIdle,
+	RightRun,
+	RightJump,
+	RightCrouch,
+
+	Roll,
+	Ladder
 };
 
 class Image;
@@ -13,34 +23,50 @@ class Animation;
 
 class Player : public GameObject
 {
-	State mState;
-	float mSpeed;
+	State mState = State::RightIdle;
+	float mSpeed = 250.f;
 	float mJumpPower;
-	float mGravity;
+	float mGravity = 0.2f;
+
+	bool invincibility = 0;
+	int isinvincible = 1;
+	bool stopmove = 0;
 
 	Image* mIdleImage;
 	Image* mRunImage;
+	Image* mTurnImage;
 	Image* mJumpImage;
 	Image* mFallImage;
 	Image* mCrouchImage;
 	Image* mRiseImage;
+	Image* mRollImage;
+	Image* mLadderUpImage;
+	Image* mLadderDownImage;
 
 	Image* mCurrentImage;
 
 	Animation* mLeftIdleAnimation;
 	Animation* mLeftRunStartAnimation;
 	Animation* mLeftRunAnimation;
+	Animation* mLeftTurnAnimation;
 	Animation* mLeftJumpAnimation;
 	Animation* mLeftFallAnimation;
 	Animation* mLeftCrouchAnimation;
 	Animation* mLeftRiseAnimation;
+	Animation* mLeftRollAnimation;
 
 	Animation* mRightIdleAnimation;
+	Animation* mRightRunStartAnimation;
 	Animation* mRightRunAnimation;
+	Animation* mRightTurnAnimation;
 	Animation* mRightJumpAnimation;
 	Animation* mRightFallAnimation;
 	Animation* mRightCrouchAnimation;
 	Animation* mRightRiseAnimation;
+	Animation* mRightRollAnimation;
+
+	Animation* mLadderUpAnimation;
+	Animation* mLadderDownAnimation;
 
 	Animation* mCurrentAnimation;	//현재 애니메이션
 
@@ -52,6 +78,8 @@ public:
 	void Update()override;
 	void Render(HDC hdc)override;
 	RECT GetRect() { return mRect; }
+
+	void SetStateRun();
 };
 
 
