f30b928f2dfec888d8bbb00a5c92c0bc50d551b9
gunmango <79182886+gunmango@users.noreply.github.com>
2021-02-23 23:57:03 +0900

    [PNH] 지팡이

---
+++ Momodora/Momodora/Staff.cpp
@@ -0,0 +1,95 @@
+#include "pch.h"
+#include "Staff.h"
+#include "Animation.h"
+#include "Image.h"
+#include "Camera.h"
+void Staff::Init()
+{
+	IMAGEMANAGER->LoadFromFile(L"Staff", Resources(L"Enemy/Witchstaff"), 256, 64, 4, 1, true);
+	mImage = IMAGEMANAGER->FindImage(L"Staff");
+
+	mReturn = false;
+	mSizeX = mImage->GetFrameWidth() * 2;
+	mSizeY = mImage->GetFrameHeight() * 2;
+
+	mRightMove = new Animation();
+	mRightMove->InitFrameByStartEnd(0, 0, 3, 0, false);
+	mRightMove->SetIsLoop(true);
+	mRightMove->SetFrameUpdateTime(0.1f);
+
+	mLeftMove = new Animation();
+	mLeftMove->InitFrameByStartEnd(0, 0, 3, 0, true);
+	mLeftMove->SetIsLoop(true);
+	mLeftMove->SetFrameUpdateTime(0.1f);
+	mCurrentAnimation = mLeftMove;
+
+}
+void Staff::Init(int x, int y, float angle)
+{
+	
+	mX = x;
+	mY = y;
+	mStart.x = x;
+	mStart.y = y;
+	mAngle = angle;
+	mSpeed = 330;
+	mReturn = false;
+	mDistance = 3;
+
+
+	if (mAngle > PI / 2)
+	{
+		mCurrentAnimation = mLeftMove;
+	}
+	else
+	{
+		mCurrentAnimation = mRightMove;
+	}
+	mCurrentAnimation->Play();
+}
+
+void Staff::Release()
+{
+	SafeDelete(mRightMove);
+	SafeDelete(mLeftMove);
+
+}
+
+void Staff::Update()
+{
+	mX += cosf(mAngle) * mSpeed * TIME->DeltaTime();
+	mY -= sinf(mAngle) * mSpeed * TIME->DeltaTime();
+
+	//초새기
+	mDistance-=TIME->DeltaTime();
+	
+	//4초후 방향전환
+	if (mDistance < 0 && !mReturn)
+	{
+		mReturn = true;
+		mSpeed *= -1;
+		
+		//if (mCurrentAnimation == mLeftMove)
+		//{
+		//	mCurrentAnimation->Stop();
+		//	mCurrentAnimation = mRightMove;
+		//	mCurrentAnimation->Play();
+		//}
+		//else
+		//{
+		//	mCurrentAnimation->Stop();
+		//	mCurrentAnimation = mLeftMove;
+		//	mCurrentAnimation->Play();
+		//}
+	}
+	
+	
+	mRect = RectMakeCenter(mX, mY, mSizeX, mSizeY);
+	mCurrentAnimation->Update();
+}
+
+
+void Staff::Render(HDC hdc)
+{
+	CAMERAMANAGER->GetMainCamera()->ScaleFrameRender(hdc, mImage, mRect.left, mRect.top, mCurrentAnimation->GetNowFrameX(), mCurrentAnimation->GetNowFrameY(), mSizeX, mSizeY);
+}
---
+++ Momodora/Momodora/Staff.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "GameObject.h"
+class Animation;
+class Image;
+class Staff :
+	public GameObject
+{
+
+	Image* mImage;
+	Animation* mLeftMove;
+	Animation* mRightMove;
+	Animation* mCurrentAnimation;
+
+	float mAngle;
+	float mSpeed;
+	RECT mPrevRect;
+	POINT mStart;
+	float mDistance; //지팡이이동거리
+	bool mReturn;
+
+public:
+	void Init()override;
+	void Init(int x, int y, float angle);
+	void Release()override;
+	void Update()override;
+	void Render(HDC hdc)override;
+	bool GetIsReturn() { return mReturn; }
+};
+
+
